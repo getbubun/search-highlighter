021a275a92d7b1b7e0f2a0dc52fe621962e778de
navin.kiit.nk@gmail.com <navin.kiit.nk@gmail.com>
2019-07-22 12:47:11 +0530

    okey

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import {FormsModule} from '@angular/forms';
-import {NgxPaginationModule} from 'ngx-pagination';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { FormsModule } from "@angular/forms";
+import { NgxPaginationModule } from "ngx-pagination";
+//test
+import { AppComponent } from "./app.component";
+import { UserService } from "./user.service";
 
-import { AppComponent } from './app.component';
-import { UserService } from './user.service';
-
-import { FilterPipe } from './filter.pipe';
-import { HighlightPipe } from './highlight.pipe';
+import { FilterPipe } from "./filter.pipe";
+import { HighlightPipe } from "./highlight.pipe";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    FilterPipe,
-    HighlightPipe
-  ],
-  imports: [
-    BrowserModule,
-    HttpClientModule,
-    FormsModule,
-    NgxPaginationModule
-  ],
+  declarations: [AppComponent, FilterPipe, HighlightPipe],
+  imports: [BrowserModule, HttpClientModule, FormsModule, NgxPaginationModule],
   providers: [UserService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
