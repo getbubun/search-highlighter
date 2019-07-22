79c93545d3626010a5b319834287e0c5c12baa46
navin.kiit.nk@gmail.com <navin.kiit.nk@gmail.com>
2019-07-22 13:23:23 +0530

    okey fine

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,10 @@ import { NgModule } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule } from "@angular/forms";
 import { NgxPaginationModule } from "ngx-pagination";
-<<<<<<< HEAD
 //test
 import { AppComponent } from "./app.component";
 import { UserService } from "./user.service";
 
-=======
-
-import { AppComponent } from "./app.component";
-import { UserService } from "./user.servi";
-
->>>>>>> 14591cc822d6cfe7e9c4193d5df532db7a8428b8
 import { FilterPipe } from "./filter.pipe";
 import { HighlightPipe } from "./highlight.pipe";
 
